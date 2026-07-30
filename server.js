5c70948b234ef3dc33d3ab19183bc7bd2d29d61b
Uchechukwu Odoemelam <odoemelam1998@gmail.com>
2026-07-30 14:55:17 +0100

    fix: bind to 0.0.0.0 and explicit index.html serving

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname), { index: 'index.html' }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Quatech site running on port ${PORT}`);
 });
